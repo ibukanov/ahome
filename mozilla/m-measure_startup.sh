7e5f67e3e3300f78740e8fc68562fc14f3c3f8ba
Igor Bukanov <igor@mir2.org>
2023-01-26 11:12:13 +0100

    Move out and ssh key confirm
    Move unrelated to IT files outside of this repo.
    
    Change ssh setup to use encrypted keys and ask for key usage confirmation.

--- mozilla/m-measure_startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-start=$(date +%s.%N)
-NO_EM_RESTART=1 \
-  exec $HOME/b/ff/tmopt/dist/bin/firefox \
-  "file:///$HOME/a/scripts/m-measure_startup.html?s=$start"
--- qubes/mtiplex/mtiplex.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <event2/event.h>
-
-int main() {
-
-    return EXIT_SUCCESS;
-}
