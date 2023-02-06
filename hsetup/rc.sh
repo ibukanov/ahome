4b29fc9b487d39a182611dd80cfc7023e10a7166
Igor Bukanov <igor@mir2.org>
2023-02-06 13:12:58 +0100

    u- as socket prefix for ssh agent

--- hsetup/rc.sh
+++ hsetup/rc.sh
@@ -20,21 +20,11 @@ if rc_is_bash; then
   test -f /etc/bashrc && . /etc/bashrc
 fi
 
-rc_set_env() {
-  local name value
-  name="$1"
-  value="$2"
-
-  # Even dash supports export a=b, so use that, not the eval hack
-  #eval $name='"$value"'
-  export "$name=$value"
-}
-
 rc_setup_env() {
 
-  #rc_set_env LANG en_US.UTF-8
-  rc_set_env LANG C.UTF-8
-  #rc_set_env LC_ALL C
+  #export LANG=en_US.UTF-8
+  export LANG=C.UTF-8
+  #export LC_ALL C
 
   if test -f "$HOME/.local/hsetup/env"; then
     . "$HOME/.local/hsetup/env"
@@ -42,14 +32,14 @@ rc_setup_env() {
 
   if test "${XDG_RUNTIME_DIR-}"; then
     local default_agent_link
-    default_agent_link="$XDG_RUNTIME_DIR/ssh-agent.socket"
+    default_agent_link="$XDG_RUNTIME_DIR/u-ssh-agent.socket"
     if test "${DISPLAY-}"; then
-      rc_set_env SSH_AUTH_SOCK "$default_agent_link"
-      rc_set_env SSH_ASKPASS u-ssh-askpass
-      rc_set_env SSH_ASKPASS_REQUIRE prefer
-      #rc_set_env SSH_ASKPASS_REQUIRE force
+      export SSH_AUTH_SOCK="$default_agent_link"
+      export SSH_ASKPASS="$HSETUP_TOP/bin/u-ssh-askpass"
+      #export SSH_ASKPASS_REQUIRE=prefer
+      export  SSH_ASKPASS_REQUIRE=force
     elif ! test "${SSH_AUTH_SOCK-}"; then
-      rc_set_env SSH_AUTH_SOCK "$default_agent_link"
+      export SSH_AUTH_SOCK="$default_agent_link"
       u-ssh-ensure-agent &
       if command -v disown >/dev/null; then
         disown
@@ -97,22 +87,22 @@ rc_history() {
 }
 
 rc_print_git_branch_for_prompt() {
-	local branch
+  local branch
   # Filter rev-parse result to avoid surprises as this goes to the terminal
   branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -dc 'a-zA-Z0-9 .:%=/_-' || :)"
-	test "$branch" || return 0
-	printf ' (%s)' "$branch"
+  test "$branch" || return 0
+  printf ' (%s)' "$branch"
 }
 
 rc_prompt() {
-	local s
+  local s
   s=
 
-	if rc_is_zsh; then
+  if rc_is_zsh; then
     setopt PROMPT_SUBST
     s="$s"'%B%F{green}%n@%m%f%b:%B%F{blue}%~%f%b'
 
-  	# Add Git branch
+    # Add Git branch
     s="$s"'%B%F{green}$(rc_print_git_branch_for_prompt)%f%b'
 
     s="$s"$'\n%B$%b '
@@ -121,16 +111,15 @@ rc_prompt() {
       s="$s"$'%{\e]0;%n@%m: %~\a%}'
     fi
 
-  	PS1="$s"
-
+    PS1="$s"
     return 0
   fi
 
   local color_prompt
   color_prompt=
-	# set a fancy prompt (non-color, unless we know we "want" color)
-	case "$TERM" in
-	xterm-color|*-256color) color_prompt=1;;
+  # set a fancy prompt (non-color, unless we know we "want" color)
+  case "$TERM" in
+  xterm-color|*-256color) color_prompt=1;;
   * )
     if test -x /usr/bin/tput && tput setaf 1 >/dev/null 2>&1; then
       # We have color support; assume it's compliant with Ecma-48
@@ -138,7 +127,7 @@ rc_prompt() {
       # a case would tend to support setf rather than setaf.)
       color_prompt=1
     fi
-	esac
+  esac
 
   if test "$color_prompt"; then
     s="$s"'\[\e[01;32m\]\u@\h\[\e[00m\]:\[\e[01;34m\]\w\[\e[00m\]'
@@ -146,14 +135,14 @@ rc_prompt() {
     s="$s"'\u@\h:\w'
   fi
 
-	# Add Git branch
-	if test "$color_prompt"; then
-		s="$s"'\[\e[01;32m\]'
-	fi
+  # Add Git branch
+  if test "$color_prompt"; then
+    s="$s"'\[\e[01;32m\]'
+  fi
   s="$s"'$(rc_print_git_branch_for_prompt)'
-	if test "$color_prompt"; then
-		s="$s"'\[\e[00m\]'
-	fi
+  if test "$color_prompt"; then
+    s="$s"'\[\e[00m\]'
+  fi
 
   s="$s"'\n\$ '
 
@@ -165,12 +154,12 @@ rc_prompt() {
   *) ;;
   esac
 
-	PS1="$s"
+  PS1="$s"
 }
 
 rc_aliases() {
-	# enable color support of ls and also add handy aliases
-	if test -x /usr/bin/dircolors; then
+  # enable color support of ls and also add handy aliases
+  if test -x /usr/bin/dircolors; then
     local s
     if test -r "$HOME/.dircolors"; then
       s="$(dircolors -b "$HOME/.dircolors")"
@@ -190,16 +179,16 @@ rc_aliases() {
 }
 
 rc_completion() {
-	# enable programmable completion features (you don't need to enable
-	# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-	# sources /etc/bash.bashrc).
-	if rc_is_bash && ! shopt -oq posix; then
-	  if test -f /usr/share/bash-completion/bash_completion; then
-	    . /usr/share/bash-completion/bash_completion
-	  elif test -f /etc/bash_completion; then
-	    . /etc/bash_completion
-	  fi
-	fi
+  # enable programmable completion features (you don't need to enable
+  # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+  # sources /etc/bash.bashrc).
+  if rc_is_bash && ! shopt -oq posix; then
+    if test -f /usr/share/bash-completion/bash_completion; then
+      . /usr/share/bash-completion/bash_completion
+    elif test -f /etc/bash_completion; then
+      . /etc/bash_completion
+    fi
+  fi
 }
 
 rc_misc() {
